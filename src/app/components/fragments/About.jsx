69a82e079e16a804b007a43eac7579b0319b6dc5
RizkiMaula <Rmaulana12321@gmail.com>
2025-02-08 17:00:09 +0700

    about yang disesuaikan

--- src/app/components/fragments/About.jsx
+++ src/app/components/fragments/About.jsx
@@ -11,9 +11,9 @@ export const About = () => {
       className="hero min-h-screen md:h-screen bg-base-100 flex flex-col gap-4 justify-center items-center px-10 "
     >
       <h1>About The Idol</h1>
-      <div className="flex flex-col-reverse m-2 md:flex-row gap-4 items-center">
+      <div className="flex flex-col-reverse m-4 md:flex-row gap-4 items-center text-sm md:text-xl">
         <div className="flex flex-col gap-4 text-justify">
-          <p className="">
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum et dolorem vel veniam earum similique velit nemo sed totam eos aliquam aspernatur, maiores quia reprehenderit molestias veritatis explicabo atque nesciunt
             perferendis voluptate.
           </p>
@@ -31,7 +31,7 @@ export const About = () => {
         <img
           src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="gello"
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="w-96 rounded-lg shadow-2xl"
         />
       </div>
     </motion.div>
